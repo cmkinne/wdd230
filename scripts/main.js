9c86d0e599256ba8bea6c4d0597a7c204bac920e
cmkinne <ckinne4416@gmail.com>
2022-09-19 17:37:47 -0700

    updated last modified date to show

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,4 @@
 
 document.getElementById("copyrightyear").innerHTML = new Date().getFullYear()
 
-document.getElementById("lastupdated").innerHTML = new Date(document.lastModified)
+document.getElementById("lastupdated").innerHTML = new Date(document.lastModified).toLocaleString()
