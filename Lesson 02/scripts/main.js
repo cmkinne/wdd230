f06f3bd3f32e84cf509875d05aef87a1536c6530
cmkinne <ckinne4416@gmail.com>
2022-09-23 21:02:14 -0700

    Lesson 2 content framework and minor cleanup

---
+++ Lesson 02/scripts/main.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById("copyrightyear2").innerHTML = new Date().getFullYear()
+
+document.getElementById("lastupdated2").innerHTML = new Date(document.lastModified).toLocaleString()
