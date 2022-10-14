ccfb4db1320a8f53d457abc03cd3f2b99c4e3f92
cmkinne <ckinne4416@gmail.com>
2022-10-13 19:44:02 -0700

    added DOM assignment

---
+++ lesson5/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const list = document.querySelector("ul")
+
+
+button.addEventListener('click', () => {
+    const favBook = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = favBook;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'X';
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+
+    input.focus();
+});
