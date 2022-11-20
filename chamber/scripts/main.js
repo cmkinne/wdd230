356f7b20ab05660df498e906d0e0ef8f9646619b
cmkinne <ckinne4416@gmail.com>
2022-11-19 21:26:56 -0700

    updated chamber page

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -7,6 +7,43 @@ x.onclick = toggleMenu;
 
 
 
-document.getElementById("coprightYear").innerHTML = new Date().getFullYear()
+document.getElementById("copyrightYear").innerHTML = new Date().getFullYear()
 
 document.getElementById("lastUpdated").innerHTML = new Date(document.lastModified).toLocaleString()
+
+
+//weather API
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 
+    'http://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&APPID=437e346f1d953d63444305b1748afbd3';
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    }
+    
+apiFetch();
