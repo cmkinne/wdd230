0e669dbf4b86699776f7fca0127ca71969f6c77f
cmkinne <ckinne4416@gmail.com>
2022-11-20 14:23:26 -0700

    made weather js https

--- lesson10/weatherAPI/weather-api.js
+++ lesson10/weatherAPI/weather-api.js
@@ -4,12 +4,12 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 const url = 
-    'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=437e346f1d953d63444305b1748afbd3';
+    'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=437e346f1d953d63444305b1748afbd3';
 
 function  displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
     
-    const iconsrc = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
     
     weatherIcon.setAttribute('src', iconsrc);
