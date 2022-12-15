59a3df441af1cc165e956009c9b443c0465df483
cmkinne <ckinne4416@gmail.com>
2022-12-14 22:59:47 -0700

    change to folder contents

--- lesson2/scripts/main.js
+++
@@ -1,5 +0,0 @@
-
-
-document.getElementById("copyrightyear2").innerHTML = new Date().getFullYear()
-
-document.getElementById("lastupdated2").innerHTML = new Date(document.lastModified).toLocaleString()
--- lesson3/scripts/main.js
+++
@@ -1,5 +0,0 @@
-
-
-document.getElementById("copyrightyear").innerHTML = new Date().getFullYear()
-
-document.getElementById("lastupdated").innerHTML = new Date(document.lastModified).toLocaleString()
